bdd7f5c23fc15af701dd2051a114e49008e930be
Kanaestephe <Kanaestephe@gmail.com>
2022-12-31 20:48:03 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,8 @@ output "nexus_server_url" {
     value = join ("", ["http://", aws_instance.nexus_instance.public_dns, ":", "8081"])
 }
 
-output "nexus_server_public_ip" {
-    value = aws_instance.nexus_instance.public_ip
+output "ssh_connection" {
+    value = join ("", ["ssh -i nexus_key_pair.pem ec2-user@", aws_instance.nexus_instance.public_dns])
 }
 
 output "nexus_admin_password" {
